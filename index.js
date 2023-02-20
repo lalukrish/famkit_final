ff18c86f050b15e4960a6c79bbcc8fe23177fc3a
Lal krishna N C <lalukrishnavallil01@gmail.com>
2023-02-20 16:12:56 +0530

    port

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ mongoose.connect(process.env.MONGOURL, {
 });
 
 mongoose.connection.on("connected", () => {
-  console.log("connected this");
+  console.log("connected this me");
 });
 
 mongoose.connection.on("error", (err) => {
