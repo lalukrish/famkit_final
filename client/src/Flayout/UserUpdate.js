c84d31250f4cac58e2ab15db43c67955cd539912
lalukrish <lalukrishnavallil01@gmail.com>
2023-05-13 16:58:22 +0530

    fix:not working

---
+++ client/src/Flayout/UserUpdate.js
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Container,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import M from "materialize-css";
+
+const UpdateUserProfile = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch(`/users/update`, {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+      body: JSON.stringify({
+        userId: localStorage.getItem("userId"),
+        name: name,
+        email: email,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        M.toast({
+          html: "okey your profile succcesfully updated",
+          classes: "#4caf50 green",
+        });
+        // handle success case
+      })
+      .catch((error) => {
+        console.error(error);
+        M.toast({
+          html: "some error occured",
+          classes: "#red",
+        });
+        // handle error case
+      });
+  };
+
+  return (
+    <Box
+      sx={{
+        alignItems: "center",
+        display: "flex",
+        flexGrow: 1,
+        minHeight: "100%",
+      }}
+    >
+      <Container maxWidth="sm" sx={{ mt: 5 }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            textAlign: "center",
+            mt: 5,
+          }}
+        >
+          <Typography variant="h4">Update Profile</Typography>
+          {message && ( // render the message if it exists
+            <Box sx={{ mt: 2 }}>
+              <Typography color="textPrimary">{message}</Typography>
+            </Box>
+          )}
+          <Box sx={{ mt: 5 }}>
+            <form onSubmit={handleSubmit}>
+              <TextField
+                label="Name"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <TextField
+                label="Email"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Box sx={{ mt: 2 }}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  type="submit"
+                >
+                  Update
+                </Button>
+              </Box>
+            </form>
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+export default UpdateUserProfile;
